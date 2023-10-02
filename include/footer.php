40ec5db10df221e9697ac31cc762580e5472c4c1
Rémi <contact@remilemaire.fr>
2023-10-02 13:54:35 +0200

    avanement footer + resize + styles

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,17 @@
-<footer>
-    <p>ICI LE FOOTER : CONTACTS ETC</p>
+<footer id="leFooter">
+    <div id="divFooterGauche">
+        <p>Communauté d'agglomération grand sud caraibes</p>
+        <p>Rue Victor Schoelcher</p>
+        <p>Saint-Claude 97120, GUADELOUPE</p>
+    </div>
+    <div id="divFooterDroite">
+        <a href="tel:+590590996320">+590 5 90 99 63 20</a>
+        <br>
+        <a href="https://twitter.com/grandsudcaraibe"><img src="../images/footer/twitter.png" alt="Twitter"></a>
+        <a href="https://fr-fr.facebook.com/CA.GrandSudCaraibe/"><img src="../images/footer/facebook.png" width="33" height="62"></a>
+        <a href="https://gp.linkedin.com/in/grand-sud-caraïbe"><img src="../images/footer/linkedin.png"></a>
+        <a href="https://www.instagram.com/cagrandsudcaraibe/"><img src="../images/footer/insta.png"></a>
+        <a href="https://www.youtube.com/@grandsudcaraibe"><img src="../images/footer/youtube.png"></a>
+    </div>
 </footer>
 </html>
--- index.php
+++ index.php
@@ -9,40 +9,32 @@ require_once("include/header.php");
 </div>
 
 <div class="actualites">
+    <h1 id="titreActus">Les dernières actus</h1>
     <div class="conteneur">
         <div class="horizontalBloc">
-            <div class="article un">
-                Article 1
+            <div class="carte">
+                <div class="contenu">
+                    <h2 class="title">Mountain View</h2>
+                    <p class="texte">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p><button class="btn">View Trips</button>
+                </div>
             </div>
-
-            <div class="article deux">
-                Article 2
+            <div class="carte">
+                <div class="contenu">
+                    <h2 class="title">To The Beach</h2>
+                    <p class="texte">Plan your next beach trip with these fabulous destinations</p><button class="btn">View Trips</button>
+                </div>
             </div>
-
-            <div class="article trois">
-                Article 3
+            <div class="carte">
+                <div class="contenu">
+                    <h2 class="title">Desert Destinations</h2>
+                    <p class="texte">It's the desert you've always dreamed of</p><button class="btn">Book Now</button>
+                </div>
             </div>
-
-            <div class="article quatre">
-                Article 4
-            </div>
-            <div class="article quatre">
-                Article 4
-            </div>
-            <div class="article quatre">
-                Article 4
-            </div>
-            <div class="article quatre">
-                Article 4
-            </div>
-            <div class="article quatre">
-                Article 4
-            </div>
-            <div class="article quatre">
-                Article 4
-            </div>
-            <div class="article quatre">
-                Article 4
+            <div class="carte">
+                <div class="contenu">
+                    <h2 class="title">Explore The Galaxy</h2>
+                    <p class="texte">Seriously, straight up, just blast off into outer space today</p><button class="btn">Book Now</button>
+                </div>
             </div>
         </div>
     </div>
