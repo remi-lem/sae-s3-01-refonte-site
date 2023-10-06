85957e412c54c7dcf8a38e72382a12294104bc91
Rémi <contact@remilemaire.fr>
2023-10-06 10:37:01 +0200

    carte leaflet recentree ° style titre communes.php

--- javascript/communes.js
+++ javascript/communes.js
@@ -2,14 +2,13 @@
 // Code JavaScript pour créer la carte Leaflet et charger les données GeoJSON
 
 // Créez une carte Leaflet
-var carte = L.map('carteCommunes').setView([16.1296, -61.6814], 11); // Coordonnées initiales et niveau de zoom
+var carte = L.map('carteCommunes').setView([16.0191, -61.6572], 11); // Coordonnées initiales et niveau de zoom
 
 // Ajoutez une couche de fond OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(carte);
 
-// Chargez le fichier GeoJSON des communes
 fetch('json/communes.json')
     .then(function(response) {
         return response.json();
@@ -18,3 +17,24 @@ fetch('json/communes.json')
         // Ajoutez les données GeoJSON à la carte
         L.geoJSON(data).addTo(carte);
     });
+
+/*
+var geojson;
+geojson = L.geoJson('json/communes.json');
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    layer.bringToFront();
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+*/
