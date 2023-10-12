ebdadebd733c2a4e6b16ba16a074666ce526cabf
Rémi <contact@remilemaire.fr>
2023-10-12 15:01:49 +0200

    badges

--- include/footer.php
+++ include/footer.php
@@ -10,6 +10,14 @@
         <p>Rue Victor Schoelcher</p>
         <p>Saint-Claude 97120, GUADELOUPE</p>
     </div>
+    <div id="divFooterCertifs">
+        <a href="https://bff.ecoindex.fr/redirect/?url=http://remilek.cluster029.hosting.ovh.net/" target="_blank">
+            <img src="https://bff.ecoindex.fr/badge/?theme=light&url=http://remilek.cluster029.hosting.ovh.net/" alt="Ecoindex Badge">
+        </a>
+        <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fremilek.cluster029.hosting.ovh.net%2F" target="_blank">
+            <img src="https://www.w3.org/Icons/valid-html401" alt="W3C badge">
+        </a>
+    </div>
     <div id="divFooterDroite">
         <a href="tel:+590590996320">+590 5 90 99 63 20</a>
         <br>
