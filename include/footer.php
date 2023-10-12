07f958203ac84222441c606d5998b71b0a8f6f17
Rémi <contact@remilemaire.fr>
2023-10-12 15:54:57 +0200

    footer

--- include/footer.php
+++ include/footer.php
@@ -21,9 +21,9 @@
     <div id="divFooterDroite">
         <a href="tel:+590590996320">+590 5 90 99 63 20</a>
         <br>
-        <!--TODO : remettre des icones plus harmonieuses-->
+        <div id="iconesContact">
         <a href="https://twitter.com/grandsudcaraibe"><img src="images/footer/twitter.png" alt="Twitter"></a>
-        <a href="https://fr-fr.facebook.com/CA.GrandSudCaraibe/"><img src="images/footer/facebook.png" alt="Facebook" width="33" height="62"></a>
+        <a href="https://fr-fr.facebook.com/CA.GrandSudCaraibe/"><img src="images/footer/facebook.png" alt="Facebook"></a>
         <a href="https://gp.linkedin.com/in/grand-sud-caraïbe"><img src="images/footer/linkedin.png" alt="LinkedIn"></a>
         <a href="https://www.instagram.com/cagrandsudcaraibe/"><img src="images/footer/insta.png" alt="Instagram"></a>
         <a href="https://www.youtube.com/@grandsudcaraibe"><img src="images/footer/youtube.png" alt="Youtube"></a>
