f6fbe20f7850f974daa97dc291babfd7664e8a58
Rémi <contact@remilemaire.fr>
2023-10-16 10:49:29 +0200

    simplification footer

--- vie_quotidienne.php
+++ vie_quotidienne.php
@@ -3,8 +3,9 @@ $titre = "Vie quotidienne | CAGSC";
 $urlstyle = "css/vie_quotidienne.css";
 require_once("include/header.php");
 ?>
-<div class="accueil">
-</div>
+    <div id="vieQuotidienne">
+        <h1>Vie quotidienne</h1>
+    </div>
 <?php
 require_once("include/footer.php");
 ?>
