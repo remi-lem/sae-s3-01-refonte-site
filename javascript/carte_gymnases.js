3ec8e38289128915b7092b9a4054616dba696121
Rémi <contact@remilemaire.fr>
2023-10-16 14:03:55 +0200

    carte gymnases

---
+++ javascript/carte_gymnases.js
@@ -0,0 +1,69 @@
+var map = L.map('carteGymnases').setView([16.0191, -61.6572], 11);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var customOptions = {'maxWidth': '200','font': 'verdana','className' : 'custom'}
+
+
+L.marker([16.05458000,-61.76700000]).addTo(map).bindPopup("<b>Complexe Sportif du Collège Vieux Habitants",customOptions);
+L.marker([15.97287000,-61.64817000]).addTo(map).bindPopup("<b>Collège des Roches Gravées Trois Rivières",customOptions);
+L.marker([15.97725000,-61.64468000]).addTo(map).bindPopup("<b>Salle Polyvalente de l'Osbf Trois Rivières",customOptions);
+L.marker([15.97175000,-61.64837000]).addTo(map).bindPopup("<b>Gymnase Félix Eboué Trois Rivières",customOptions);
+L.marker([16.33303000,-61.70609000]).addTo(map).bindPopup("<b>Lycée de la Ramée Sainte Rose",customOptions);
+L.marker([16.33029000,-61.69999000]).addTo(map).bindPopup("<b>Hall des Sports Sainte Rose",customOptions);
+L.marker([16.22327000,-61.38996000]).addTo(map).bindPopup("<b>Studio Espace Tonic Sainte Anne",customOptions);
+L.marker([16.24825000,-61.28679000]).addTo(map).bindPopup("<b>Gymnase et Boulodrome des Raisins Clairs Saint François",customOptions);
+L.marker([16.01706000,-61.71658000]).addTo(map).bindPopup("<b>Gymnase Cassin Saint Claude",customOptions);
+L.marker([16.01519000,-61.71425000]).addTo(map).bindPopup("<b>Club Sportif de Ducharmoy Saint Claude",customOptions);
+L.marker([16.01759000,-61.71616000]).addTo(map).bindPopup("<b>Collège Rémy Nainsouta Saint Claude",customOptions);
+L.marker([16.02769000,-61.69727000]).addTo(map).bindPopup("<b>Hall des Sports Patrick Cham Saint Claude",customOptions);
+L.marker([16.41226000,-61.51524000]).addTo(map).bindPopup("<b>Lpo Nord Grande Terre Port Louis",customOptions);
+L.marker([16.22832000,-61.78461000]).addTo(map).bindPopup("<b>Hall des Sports du Bourg Pointe Noire",customOptions);
+L.marker([16.24696000,-61.53558000]).addTo(map).bindPopup("<b>Hall des Sports Paul Chonchon Pointe à Pitre",customOptions);
+L.marker([16.22587000,-61.53055000]).addTo(map).bindPopup("<b>Complexe Sportif de Fouillole Pointe à Pitre",customOptions);
+L.marker([16.23852000,-61.53788000]).addTo(map).bindPopup("<b>Collège Sadi Carnot Pointe à Pitre",customOptions);
+L.marker([16.23852000,-61.53788000]).addTo(map).bindPopup("<b>Collège Sadi Carnot Pointe à Pitre",customOptions);
+L.marker([16.24407000,-61.54000000]).addTo(map).bindPopup("<b>Centre d\'Animation Culturel de Bergevin Pointe à Pitre",customOptions);
+L.marker([16.22027000,-61.53159000]).addTo(map).bindPopup("<b>Tennis Club de la Marina Pointe à Pitre",customOptions);
+L.marker([16.24210000,-61.53822000]).addTo(map).bindPopup("<b>Gymnase de Chanzy Pointe à Pitre",customOptions);
+L.marker([16.38407000,-61.48654000]).addTo(map).bindPopup("<b>Complexe Sportif Cyrano Arondel Petit Canal",customOptions);
+L.marker([16.20128000,-61.59000000]).addTo(map).bindPopup("<b>Palais des Sports Laura Flessel Petit Bourg",customOptions);
+L.marker([16.33283000,-61.35276000]).addTo(map).bindPopup("<b>Collège Charles de Gaulle Le Moule",customOptions);
+L.marker([16.33260000,-61.35026000]).addTo(map).bindPopup("<b>Gymnase Félix Abouna Le Moule",customOptions);
+L.marker([16.33114000,-61.34376000]).addTo(map).bindPopup("<b>Fitness-Club du Moule Le Moule",customOptions);
+L.marker([16.33261000,-61.45674000]).addTo(map).bindPopup("<b>Stade Municipal Pierre Monnerville Morne à l'Eau",customOptions);
+L.marker([16.34269000,-61.45471000]).addTo(map).bindPopup("<b>Terrain de l'Etoile Morne à l'Eau",customOptions);
+L.marker([16.33696000,-61.44969000]).addTo(map).bindPopup("<b>Lycée Fostin Fleret Morne à l'Eau",customOptions);
+L.marker([16.26755000,-61.63721000]).addTo(map).bindPopup("<b>Gymnase de Crane Lamentin",customOptions);
+L.marker([16.27358000,-61.63149000]).addTo(map).bindPopup("<b>Complexe Sportif de Blachon Lamentin",customOptions);
+L.marker([16.21924000,-61.52008000]).addTo(map).bindPopup("<b>Palais des Sports Le Gosier",customOptions);
+L.marker([16.21966000,-61.52007000]).addTo(map).bindPopup("<b>Palais des Sports Le Gosier",customOptions);
+L.marker([16.20817000,-61.45900000]).addTo(map).bindPopup("<b>Lycée Hôtelier Le Gosier",customOptions);
+L.marker([16.20808000,-61.50673000]).addTo(map).bindPopup("<b>Hôtel Créole Beach Le Gosier",customOptions);
+L.marker([15.88716000,-61.31412000]).addTo(map).bindPopup("<b>Salle Polyvalente de Tivoli Grand Bourg",customOptions);
+L.marker([16.32624000,-61.76937000]).addTo(map).bindPopup("<b>Aire de Jeux Sportifs de Caféière Deshaies",customOptions);
+L.marker([16.31946000,-61.03410000]).addTo(map).bindPopup("<b>Gold Gym Judo Club Désiradien La Désirade",customOptions);
+L.marker([15.99177000,-61.71572000]).addTo(map).bindPopup("<b>Lycée d'Enseignement Professionnel Privé Gourbeyre",customOptions);
+L.marker([15.99218000,-61.68961000]).addTo(map).bindPopup("<b>Hall des Sports Lucette Michaux-Chevry Gourbeyre",customOptions);
+L.marker([16.04947000,-61.56086000]).addTo(map).bindPopup("<b>Gymnase G. Marianne Capesterre Belle Eau",customOptions);
+L.marker([16.12967000,-61.76891000]).addTo(map).bindPopup("<b>Hall des Sports Gilles Echevin Bouillante",customOptions);
+L.marker([15.99160000,-61.72557000]).addTo(map).bindPopup("<b>Lycée Gerville Réache Basse Terre",customOptions);
+L.marker([15.99359000,-61.72824000]).addTo(map).bindPopup("<b>La Gauloise Palais Louis André Basse Terre",customOptions);
+L.marker([16.00637000,-61.74135000]).addTo(map).bindPopup("<b>Palais des Sports Régional Basse Terre",customOptions);
+L.marker([16.02158000,-61.74627000]).addTo(map).bindPopup("<b>Salle des Sports Bo'Sport - Association la Baillifienne Baillif",customOptions);
+L.marker([16.01402000,-61.73765000]).addTo(map).bindPopup("<b>Complexe Sportif du Cygne Noir Pierre Martin Baillif",customOptions);
+L.marker([16.26727000,-61.58118000]).addTo(map).bindPopup("<b>Lycée Charles Coeffin Baie Mahault",customOptions);
+L.marker([16.24719000,-61.59608000]).addTo(map).bindPopup("<b>Collège Gourdeliane Baie Mahault",customOptions);
+L.marker([16.25407000,-61.60402000]).addTo(map).bindPopup("<b>Fond Budan Baie Mahault",customOptions);
+L.marker([16.26390000,-61.57414000]).addTo(map).bindPopup("<b>Centre Pénitentiaire Baie Mahault",customOptions);
+L.marker([16.25714000,-61.53076000]).addTo(map).bindPopup("<b>Collège Raizet Les Abymes",customOptions);
+L.marker([16.24689000, 61.53512000]).addTo(map).bindPopup("<b>Lycée Baimbridge Les Abymes",customOptions);
+L.marker([16.24711000, 61.52332000]).addTo(map).bindPopup("<b>Lycée Baimbridge Les Abymes",customOptions);
+L.marker([16.26929000,-61.50897000]).addTo(map).bindPopup("<b>Complexe Sportif Vieux-Bourg Abymes Les Abymes",customOptions);
+L.marker([16.25892000,-61.51733000]).addTo(map).bindPopup("<b>Caserne de Petit-Pérou Les Abymes",customOptions);
+L.marker([16.26944000,-61.50988000]).addTo(map).bindPopup("<b>Grand Gymnase de Dothémare Les Abymes",customOptions);
+L.marker([16.26974000,-61.50984000]).addTo(map).bindPopup("<b>Petit Gymnase de Dothemare Les Abymes",customOptions);
+L.marker([16.25980000,-61.52037000]).addTo(map).bindPopup("<b>Creps Antilles Guyane Les Abymes",customOptions);
+L.marker([16.25180000,-61.53418000]).addTo(map).bindPopup("<b>Shape Up Nubret Les Abymes",customOptions);
--- vie_quotidienne.php
+++ vie_quotidienne.php
@@ -7,26 +7,49 @@ require_once("include/header.php");
         <h1>Vie quotidienne</h1>
     </div>
     <div id="demarches">
-        <h2>Liens utiles</h2>
+        <h2>En bref</h2>
         <div id="cartes">
             <div class="row">
-                <div class="col-sm-6 mb-3 mb-sm-0">
+                <div class="col-sm">
                     <div class="card">
                         <img src="images/vie_quotidienne/bus.jpg" class="card-img-top" alt="Autobus">
                         <div class="card-body">
                             <h5 class="card-title">Horaires de bus</h5>
-                            <p class="card-text">Consulter les horaires de bus</p>
+                            <p class="card-text">Vous pouvez consulter les horaires de bus sur l'application</p>
                             <a href="https://play.google.com/store/apps/details?id=sg.emsystem.eM_USR_Android_TSUD" class="btn btn-primary">Application Android</a>
                             <a href="https://apps.apple.com/fr/app/trans-sud/id6449498067" class="btn btn-primary">Application iOS</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-6">
-                <div class="card">
+                <div class="col-sm">
+                    <div class="card">
                         <img src="images/vie_quotidienne/bus.jpg" class="card-img-top" alt="Autobus">
                         <div class="card-body">
-                            <h5 class="card-title">titre</h5>
-                            <p class="card-text">texte</p>
+                            <!-- TODO -->
+                            <h5 class="card-title">Addresses des gymnases</h5>
+                            <p class="card-text">Voici la carte des gymnases ou vous pouvez faire du sport</p>
+                            <a href="#" class="btn btn-primary">lien</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <div class="card">
+                        <img src="images/vie_quotidienne/bus.jpg" class="card-img-top" alt="Autobus">
+                        <div class="card-body">
+                            <!-- TODO -->
+                            <h5 class="card-title">loisirs</h5>
+                            <p class="card-text">Voici la carte des gymnases ou vous pouvez faire du sport</p>
+                            <a href="#" class="btn btn-primary">lien</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <div class="card">
+                        <img src="images/vie_quotidienne/bus.jpg" class="card-img-top" alt="Autobus">
+                        <div class="card-body">
+                            <!-- TODO -->
+                            <h5 class="card-title">culture</h5>
+                            <p class="card-text">Voici la carte des gymnases ou vous pouvez faire du sport</p>
                             <a href="#" class="btn btn-primary">lien</a>
                         </div>
                     </div>
@@ -35,6 +58,15 @@ require_once("include/header.php");
             </div>
 
         </div>
+<div id="gymnases">
+    <h2>Les gymnases</h2>
+    <div id="conteneurCarteGym">
+        <div id="carteGymnases"></div>
+    </div>
+</div>
+<script src="javascript/carte_gymnases.js"></script>
+
+
 <?php
 require_once("include/footer.php");
 ?>
